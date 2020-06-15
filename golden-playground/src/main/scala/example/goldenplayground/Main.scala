cc50af4d57beb2a635406eedeaab6ce834a8715f
shinharad <shin.harad@gmail.com>
2020-06-15 09:37:56 +0900

    golden testing

--- golden-playground/src/main/scala/example/goldenplayground/Main.scala
+++ golden-playground/src/main/scala/example/goldenplayground/Main.scala
@@ -7,10 +7,17 @@ import cats.implicits._
 import io.circe._
 import io.circe.generic.semiauto._
 
-final case class Person(name: String, age: Int, email: String)
+final case class Person(name: String, age: Int)
+// final case class Person(name: String, age: Int, email: String)
 object Person {
   implicit val eq: Eq[Person] = Eq.fromUniversalEquals
   implicit val codec: Codec[Person] = deriveCodec
+}
+
+final case class Data(char: Char, boolean: Boolean)
+object Data {
+  implicit val eq: Eq[Data] = Eq.fromUniversalEquals
+  implicit val codec: Codec[Data] = deriveCodec
 }
 
 object Main extends App {
@@ -19,7 +26,8 @@ object Main extends App {
   import io.circe.syntax._
   import io.circe.parser._
 
-  Person("Alice", 27, "a@b.com")
+  Person("Alice", 27)
+  // Person("Alice", 27, "a@b.com")
     .asJson
     .spaces2
     .tap(printlnGood)
--- golden-playground/src/test/scala/example/goldenplayground/ExampleJsonSuite.scala
+++ golden-playground/src/test/scala/example/goldenplayground/ExampleJsonSuite.scala
@@ -2,5 +2,8 @@ package example.goldenplayground
 
 final class ExampleJsonSuite extends JsonTestSuite {
   // checkAll("Cocec[Person]", CodecTests[Person].codec)
+
+  // Golden Testing
   checkAll("GoldenCocec[Person]", GoldenCodecTests[Person].goldenCodec)
+  checkAll("GoldenCocec[Data]", GoldenCodecTests[Data].goldenCodec)
 }
